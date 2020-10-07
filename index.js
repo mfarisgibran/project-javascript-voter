8b120a094814ce476e0884e7b9db1f762de8cd5c
M Faris Gibran <mfarisgibran@gmail.com>
2020-10-07 17:45:02 +0700

    Convert result in percentage

--- index.js
+++ index.js
@@ -12,13 +12,23 @@ const result2Element = document.getElementById("result-2");
 function voteOption1() {
   votes.option1 += 1;
 
-  result1Element.innerText = votes.option1;
+  let resultInPercentage =
+    (votes.option1 / (votes.option1 + votes.option2)) * 100;
+
+  let roundedPercentage = resultInPercentage.toFixed(2);
+
+  result1Element.innerText = roundedPercentage + "%";
 }
 
 function voteOption2() {
   votes.option2 += 1;
 
-  result2Element.innerText = votes.option2;
+  let resultInPercentage =
+    (votes.option2 / (votes.option1 + votes.option2)) * 100;
+
+  let roundedPercentage = resultInPercentage.toFixed(2);
+
+  result2Element.innerText = roundedPercentage + "%";
 }
 
 option1ButtonElement.addEventListener("click", voteOption1);
