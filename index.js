50296681799c7da6ac8248b5136a7202b9585640
M Faris Gibran <mfarisgibran@gmail.com>
2020-10-07 17:29:58 +0700

    Modify result with actual updated value

--- index.js
+++ index.js
@@ -3,16 +3,23 @@ const votes = {
   option2: 0,
 };
 
+const option1ButtonElement = document.getElementById("option-1");
+const option2ButtonElement = document.getElementById("option-2");
+
+const result1Element = document.getElementById("result-1");
+const result2Element = document.getElementById("result-2");
+
 function voteOption1() {
   votes.option1 += 1;
+
+  result1Element.innerText = votes.option1;
 }
 
 function voteOption2() {
   votes.option2 += 1;
+
+  result2Element.innerText = votes.option2;
 }
 
-const option1Button = document.getElementById("option-1");
-const option2Button = document.getElementById("option-2");
-
-option1Button.addEventListener("click", voteOption1);
-option2Button.addEventListener("click", voteOption2);
+option1ButtonElement.addEventListener("click", voteOption1);
+option2ButtonElement.addEventListener("click", voteOption2);
